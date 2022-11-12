0e0135457f794f9ee7e399d1a98dfffa25584d38
Panna <33679809+pawanyoda@users.noreply.github.com>
2022-11-12 09:13:49 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='test_package_by_pawan',
+    packages=['test'],
+    description='test package',
+    version='1.0.0',
+    author='Pawan',
+    author_email='pawan.yoda@gmail.com',
+    keywords='test package'
+)
